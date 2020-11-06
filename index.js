9933b0278fab64a68925dc497b0170d6a57784e2
waiizen <nathanferrer.nf@gmail.com>
2020-11-06 12:19:07 +0100

    end city search basics of app' informations

--- index.js
+++ index.js
@@ -1,17 +1,82 @@
 $(document).ready(function(){
 
-    var liste = [];
+    // ##### VARIABLES GLOBALES #####
 
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?appid=" + config.MY_KEY;
+    var liste = [];
+    var villeMeteoID = '';
+    var villeMeteo = '';
+    var temperature, tempRessenti, tempMini, tempMax, ventVitesse, humidite = 0;
+    var tempsTxt = '';
+
+    // ##### PARTIE METEO D'UNE VILLE #####
+
+    function meteo(){
+        let url = apiUrl + '&id=' + villeMeteoID + '&units=metric' + '&lang=fr';
+        fetch(url, {method: "GET"})
+            .then(response => response.json())
+            .then(DATAJSON => {
+                console.log(DATAJSON);
+                temperature = DATAJSON.main.temp;
+                tempRessenti = DATAJSON.main.feels_like;
+                tempMini = DATAJSON.main.temp_min;
+                tempMax = DATAJSON.main.temp_max;
+                ventVitesse = DATAJSON.wind.speed;
+                humidite = DATAJSON.main.humidity;
+                tempsTxt = DATAJSON.weather[0].description;
+                affichageApp();
+            })
+            .catch(error => alert("Erreur: " + error));
+    }
+
+    function affichageApp(){
+        let resultat = '<p>' + villeMeteo + ' ' + villeMeteoID + '<br/>'
+            + 'temperature ' + temperature + '<br/>'
+            + 'tempRessenti ' +  tempRessenti + '<br/>'
+            + 'tempMini ' +  tempMini + '<br/>'
+            + 'tempMax ' +  tempMax + '<br/>'
+            + 'ventVitesse ' +  ventVitesse + '<br/>'
+            + 'humidite ' +  humidite + '<br/>'
+            + 'tempsTxt ' +  tempsTxt + '</p>';
+
+        $('.app').append(resultat);
+    }
+
+    // ##### PARTIE RECHERCHE D'UNE VILLE #####
+
+    // clique sur un bouton
+    $('.resultat').on('click', 'button', function(){
+        let splt = this.id.split(";");
+        villeMeteoID = splt[0];
+        villeMeteo = splt[1];
+        $('#modalVille').modal('hide');
+        meteo();
+    });
+
+    // recherche de la ville a chaque lettre entrée
     $('input[name=choisirVille]').keyup(function(){
-        if($('input[name=choisirVille]').val().length > 3){
-            clear();
-            var villeRecherche = format($('input[name=choisirVille]').val());
-            $.get("city.list.json", function(data){
-                for(i in data) if(data[i].name.includes(villeRecherche)) creation(data[i].id, data[i].country, data[i].name);
-                affichage();
-            });
-        }
+        delay(function(){
+            if ($('input[name=choisirVille]').val().length > 3) {
+                clear();
+                let villeRecherche = format($('input[name=choisirVille]').val());
+                $.get("city.list.json", function (data) {
+                    for (i in data) if (data[i].name.includes(villeRecherche)) creation(data[i].id, data[i].country, data[i].name);
+                    affichageVilles();
+                });
+            } else {
+                clear();
+            }
+        }, 500);
     })
+
+    // attendre que l'utilisateur n'ai pas entré de touche pendant Xms
+    var delay = (function(){
+        var timer = 0;
+        return function(callback, ms){
+            clearTimeout (timer);
+            timer = setTimeout(callback, ms);
+        };
+    })();
 
     function clear(){
         liste = [];
@@ -22,24 +87,23 @@ $(document).ready(function(){
     function tri(){
         liste.sort();
         for(var i = 1 ; i < liste.length ; i++){
-            var splt = liste[i].split(";");
-            var spltBefore = liste[i-1].split(";");
+            let splt = liste[i].split(";");
+            let spltBefore = liste[i-1].split(";");
             if(splt[0] == spltBefore[0] && splt[1] == spltBefore[1]){
                 liste.splice(i,1);
             }
         }
     }
 
-    function affichage(){
+    function affichageVilles(){
         tri();
         var resultat = '<table class="table table-striped table-bordered table-hover">';
-        for(var i = 0 ; i < liste.length ; i++){
+        for(let i = 0 ; i < liste.length ; i++){
             resultat += '<tr class="trVille">';
             var splt = liste[i].split(";")
             resultat += '<td>' + splt[0] + '</td>';
             resultat += '<td>' + splt[1] + '</td>';
-            resultat += '<td style="display:none;" class="codeVille">' + splt[2] + '</td>';
-            resultat += '<td><button type="button" class="btn btn-primary">Primary</button></td>';
+            resultat += '<td><button type="button" class="btn btn-info" id="' + splt[2] + ';' + splt[1] + '">Choisir</button></td>';
             resultat += '</tr>';
         }
         resultat += '</table>';
@@ -47,44 +111,11 @@ $(document).ready(function(){
     }
 
     function creation(id, pays, ville){
-        var send = pays + ';' + ville + ';' + id
+        let send = pays + ';' + ville + ';' + id
         liste.push(send);
     }
 
     function format(text){
         return text.charAt(0).toUpperCase() + text.slice(1);
-    }
-
-    function ville(){
-        var villeRecherche = $('input[name=choisirVille]').val();
-        $.get("city.list.json", function(data){
-            console.log('commence');
-            for(i in data){
-                if(data[i].name.includes(villeRecherche)){
-                    console.log(data[i].name, data[i].country);
-                }
-            }
-            console.log('fini');
-        });
-    }
-
-    function letsgo(){
-        console.log("lets go");
-        var url = "http://api.openweathermap.org/data/2.5/weather?appid=" + config.MY_KEY;
-
-        url += '&zip=';
-        url += $('input[name=choisirVille]').val();
-
-        fetch(url, {method: "GET"})
-            .then(response => response.json())
-            .then(DATAJSON => {
-                console.log(DATAJSON);
-                console.log('----');
-                for(i in DATAJSON){
-                    console.log(DATAJSON.name);
-                }
-
-            })
-            .catch(error => alert("Erreur: " + error));
     }
 });
